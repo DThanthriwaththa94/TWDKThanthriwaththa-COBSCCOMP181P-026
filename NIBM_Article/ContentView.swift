27a1b5aa9168c6f6fae8b2d719833e4bff536319
bagya-hennayake <bagyasrnat@gmail.com>
2019-11-22 20:48:42 +0530

    Initial Commit

---
+++ NIBM_Article/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NIBM_Article
+//
+//  Created by Bagya Hennayake on 11/22/19.
+//  Copyright © 2019 NIBM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
