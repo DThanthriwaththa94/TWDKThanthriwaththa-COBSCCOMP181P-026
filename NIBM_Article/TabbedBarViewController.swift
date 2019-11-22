ba4b58c079f9ec4551ac2afa010815628fe552bc
bagya-hennayake <bagyasrnat@gmail.com>
2019-11-22 23:42:45 +0530

    tab bar controller

--- NIBM_Article/TabbedBarViewController.swift
+++ NIBM_Article/TabbedBarViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import BiometricAuthentication
 
-class TabbedBarViewController: UITabBarController {
+class TabbedBarViewController: UITabBarController, UITabBarControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.delegate = self
+        }
+        func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+            if viewController.isKind(of: MyProfileViewController.self) {
+               
+                BioMetricAuthenticator.authenticateWithBioMetrics(reason: "Authentication required to access this section") { (result) in
+                    
+                    switch result {
+                    case .success( _):
+                        print("Authentication Successful")
+                        self.selectedIndex = 2
+                    case .failure(let error):
+                        print("Authentication Failed")
+                        
+                    }
+                }
+                return false
+            }
+            return true
+        }
+        override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+            print("eeeee--\(item.tag)")
+            
+        
         // Do any additional setup after loading the view.
     }
     
